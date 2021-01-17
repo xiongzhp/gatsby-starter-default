c0b1df9753de6707c5d86e10937c9acf6ac2f188
ErikXiong <xiongzhp@outlook.com>
2021-01-17 11:22:00 +0800

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,34 +1,46 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: 'Michael J. Deeb',
+    description: 'Michael Deeb is a web developer in San Francisco, CA. This is his personal website.',
+    repositoryUrl: 'https://github.com/michaeljdeeb/michaeljdeeb.com',
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-plugin-google-analytics',
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        trackingId: 'UA-49663508-2',
+        head: true,
+        respectaDNT: true,
       },
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        name: 'pages',
+        path: `${__dirname}/src/pages/`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              maxWidth: 1000,
+            },
+          },
+          {
+            resolve: 'gatsby-remark-responsive-iframe',
+          },
+          'gatsby-remark-prismjs',
+          'gatsby-remark-copy-linked-files',
+          'gatsby-remark-smartypants',
+        ],
+      },
+    },
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
   ],
-}
+};
--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,70 @@
 /**
  * Implement Gatsby's Node APIs in this file.
  *
- * See: https://www.gatsbyjs.com/docs/node-apis/
+ * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
 // You can delete this file if you're not using it
+const path = require('path');
+
+exports.onCreateNode = ({ node, boundActionCreators, getNode }) => {
+  const { createNodeField } = boundActionCreators;
+  let slug;
+
+  if (node.internal.type === 'MarkdownRemark') {
+    const fileNode = getNode(node.parent);
+    const parsedFilePath = path.parse(fileNode.relativePath);
+
+    if (parsedFilePath.name !== 'index' && parsedFilePath.dir !== '') {
+      slug = `/${parsedFilePath.dir}/${parsedFilePath.name}/`;
+    } else if (parsedFilePath.dir === '') {
+      slug = `/${parsedFilePath.name}/`;
+    } else {
+      slug = `/${parsedFilePath.dir}/`;
+    }
+
+    createNodeField({ node, name: 'slug', value: slug });
+  }
+};
+
+exports.createPages = ({ graphql, boundActionCreators }) => {
+  const { createPage } = boundActionCreators;
+
+  return new Promise((resolve, reject) => {
+    const pages = [];
+    const blogPost = path.resolve('src/templates/blog-post.js');
+
+    resolve(
+      graphql(
+        `
+          {
+            allMarkdownRemark {
+              edges {
+                node {
+                  fields {
+                    slug
+                  }
+                }
+              }
+            }
+          }
+        `).then((result) => {
+        if (result.errors) {
+          console.log(result.errors);
+          reject(result.errors);
+        }
+
+        result.data.allMarkdownRemark.edges.forEach((edge) => {
+          // Strip yyyy-mm-dd- out of /blog/yyyy-mm-dd-post-title
+          const cleanPath = edge.node.fields.slug.replace(/(\d){4}-(\d){2}-(\d){2}-/, '');
+          createPage({
+            path: cleanPath,
+            component: blogPost,
+            context: {
+              slug: edge.node.fields.slug,
+            },
+          });
+        });
+      }));
+  });
+};
